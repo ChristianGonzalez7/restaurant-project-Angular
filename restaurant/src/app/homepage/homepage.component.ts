b721941f8a1a978baf28ae19f51ada0ad7a09010
ChristianGonzalez7 <christiangonzalez1994@gmail.com>
2020-07-20 12:43:18 +0200

    homepage update modal images

--- restaurant/src/app/homepage/homepage.component.ts
+++ restaurant/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  homepageImages: any
 
-  ngOnInit(): void {
+  constructor(private service: ServiceService) {
+    this.homepageImages = new Array;
+   }
+
+  async ngOnInit() {
+    this.homepageImages = await this.service.getHomepageImages();
+    console.log(this.homepageImages);
   }
+
+
 
 }
--- restaurant/src/app/service.service.ts
+++ restaurant/src/app/service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  homepageImagesUrl: string;
+  homepageImages: any;
+
+
+  constructor(private http: HttpClient) {
+    this.homepageImagesUrl = 'https://api.spoonacular.com/food/menuItems/search?apiKey=36b2616bbbd3463891cdf064597f778c&query=burger&number=3'
+
+  }
+
+  getHomepageImages (): Promise<any> {
+    const prom = new Promise((resolve, reject)=>{
+      this.http.get(this.homepageImagesUrl).subscribe(importInfo => {
+        this.homepageImages = importInfo;
+        resolve(this.homepageImages.menuItems);
+      })
+    })
+    return(prom);
+  }
+
+
+
 }
